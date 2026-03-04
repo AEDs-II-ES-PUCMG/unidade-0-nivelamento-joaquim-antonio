e3667dce0190eb7a29263efc570b1c34e149129f
Joaquim Antonio <joaquim.antonio.camargos@gmail.com>
2026-03-04 15:16:22 -0300

    feat: tarefa 03 e 04 implementadas

---
+++ oficina/src/main/java/com/produtos/Comercio.java
@@ -0,0 +1,198 @@
+package com.produtos;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Comercio {
+    /** Para inclusão de novos produtos no vetor */
+    static final int MAX_NOVOS_PRODUTOS = 10;
+
+    /** Nome do arquivo de dados. O arquivo deve estar localizado na raiz do projeto */
+    static String nomeArquivoDados;
+
+    /** Scanner para leitura do teclado */
+    static Scanner teclado;
+
+    /** Vetor de produtos cadastrados. Sempre terá espaço para 10 novos produtos a cada execução */
+    static Produto[] produtosCadastrados;
+
+    /** Quantidade produtos cadastrados atualmente no vetor */
+    static int quantosProdutos;
+
+    /** Gera um efeito de pausa na CLI. Espera por um enter para continuar */
+    static void pausa(){
+        System.out.println("Digite enter para continuar...");
+        teclado.nextLine();
+    }
+
+    /** Cabeçalho principal da CLI do sistema */
+    static void cabecalho(){
+        System.out.println("AEDII COMÉRCIO DE COISINHAS");
+        System.out.println("===========================");
+    }
+
+    /** Imprime o menu principal, lê a opção do usuário e a retorna (int).
+     * Perceba que poderia haver uma melhor modularização com a criação de uma classe Menu.
+     * @return Um inteiro com a opção do usuário.
+     */
+    static int menu(){
+        cabecalho();
+        System.out.println("1 - Listar todos os produtos");
+        System.out.println("2 - Procurar e listar um produto");
+        System.out.println("3 - Cadastrar novo produto");
+        System.out.println("0 - Sair");
+        System.out.print("Digite sua opção: ");
+        return Integer.parseInt(teclado.nextLine());
+    }
+
+    /**
+     * Lê os dados de um arquivo texto e retorna um vetor de produtos. Arquivo no formato
+     * N  (quantiade de produtos) <br/>
+     * tipo; descrição;preçoDeCusto;margemDeLucro;[dataDeValidade] <br/>
+     * Deve haver uma linha para cada um dos produtos. Retorna um vetor vazio em caso de problemas com o arquivo.
+     * @param nomeArquivoDados Nome do arquivo de dados a ser aberto.
+     * @return Um vetor com os produtos carregados, ou vazio em caso de problemas de leitura.
+     */
+    static Produto[] lerProdutos(String nomeArquivoDados) throws IOException {
+        Produto[] vetorProdutos;
+
+        BufferedReader leitor = new BufferedReader(new FileReader(nomeArquivoDados));
+
+        //le o numero de produtos
+        quantosProdutos = Integer.parseInt(leitor.readLine());
+        vetorProdutos = new Produto[quantosProdutos + MAX_NOVOS_PRODUTOS];
+
+        //le os outros produtos e guarda no vetor
+        String linha = "";
+        for(int i=0; i<quantosProdutos; i++){
+            linha = leitor.readLine();
+            vetorProdutos[i] = Produto.criarDoTexto(linha);
+        }
+
+        return vetorProdutos;
+    }
+
+    /** Lista todos os produtos cadastrados, numerados, um por linha */
+    static void listarTodosOsProdutos(){
+        cabecalho();
+        System.out.println("\nPRODUTOS CADASTRADOS:");
+        for (int i = 0; i < produtosCadastrados.length; i++) {
+            if(produtosCadastrados[i]!=null)
+                System.out.println(String.format("%02d - %s", (i+1),produtosCadastrados[i].toString()));
+        }
+    }
+
+    /** Localiza um produto no vetor de cadastrados, a partir do nome, e imprime seus dados.
+     *  A busca não é sensível ao caso.  Em caso de não encontrar o produto, imprime mensagem padrão */
+    static void localizarProdutos(){
+        cabecalho();
+        System.out.println("Digite o item que deseja buscar: ");
+        String itemProcurado = teclado.nextLine();
+
+        for(int i=0; i<produtosCadastrados.length; i++){
+            if(produtosCadastrados[i] != null && produtosCadastrados[i].getDescricao().toLowerCase().equals(itemProcurado.toLowerCase())){
+                System.out.println("\n\n========PRODUTO ENCONTRADO==========");
+                System.out.println(String.format("%02d - %s", (i+1),produtosCadastrados[i].toString()));
+            }
+        }
+    }
+
+    /**
+     * Rotina de cadastro de um novo produto: pergunta ao usuário o tipo do produto, lê os dados correspondentes,
+     * cria o objeto adequado de acordo com o tipo, inclui no vetor. Este método pode ser feito com um nível muito
+     * melhor de modularização. As diversas fases da lógica poderiam ser encapsuladas em outros métodos.
+     * Uma sugestão de melhoria mais significativa poderia ser o uso de padrão Factory Method para criação dos objetos.
+     */
+    static void cadastrarProduto(){
+        if(quantosProdutos != produtosCadastrados.length) {
+            int res = -1;
+            do {
+                cabecalho();
+                System.out.println("=======MENU DE CADASTRO========\nQual o tipo de produto que voce deseja cadastrar?(1)Nao perecivel (2)Perecivel");
+                int resp = teclado.nextInt();
+
+                if (resp == 1) {
+                    //cadastra nao perecivel
+                    System.out.println("Digite o nome do produto: ");
+                    teclado.nextLine();
+                    String desc = teclado.nextLine();
+                    System.out.println("Digite o preco de custo do produto: ");
+                    double precoCusto = teclado.nextDouble();
+                    System.out.println("Digite a margem de lucro do produto: ");
+                    double margemLucro = teclado.nextDouble();
+
+                    Produto novoProduto = new ProdutoNaoPerecivel(desc, precoCusto, margemLucro);
+                    produtosCadastrados[quantosProdutos] = novoProduto;
+                    quantosProdutos++;
+
+                } else if (resp == 2) {
+                    //cadastra perecivel
+                    DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+                    System.out.println("Digite o nome do produto: ");
+                    teclado.nextLine();
+                    String desc = teclado.nextLine();
+                    System.out.println("Digite o preco de custo do produto: ");
+                    double precoCusto = teclado.nextDouble();
+                    System.out.println("Digite a margem de lucro do produto: ");
+                    double margemLucro = teclado.nextDouble();
+                    System.out.println("Digite a data de validade no padrao dd/MM/yyyy: ");
+                    teclado.nextLine();
+                    String dataValidade = teclado.nextLine();
+
+                    Produto novoProduto = new ProdutoPerecivel(desc, precoCusto, margemLucro, LocalDate.parse(dataValidade, formato));
+                    produtosCadastrados[quantosProdutos] = novoProduto;
+                    quantosProdutos++;
+
+                } else
+                    System.out.println("\nResposta invalida");
+
+                System.out.println("Deseja cadastrar um produto? Ainda podem ser cadastrados " + (produtosCadastrados.length - quantosProdutos) + " produtos: (1)sim (0)não");
+                res = teclado.nextInt();
+            } while (res == 1);
+        }else
+            System.out.println("NAO PODEM SER CADASTRADOS MAIS PRODUTOS");
+    }
+
+    /**
+     * Salva os dados dos produtos cadastrados no arquivo csv informado. Sobrescreve todo o conteúdo do arquivo.
+     * @param nomeArquivo Nome do arquivo a ser gravado.
+     */
+    public static void salvarProdutos(String nomeArquivo) throws IOException {
+        BufferedWriter escritor = new BufferedWriter(new FileWriter(nomeArquivo));
+
+        escritor.write(String.valueOf(quantosProdutos));
+        escritor.newLine();
+
+        for(int i=0; i<quantosProdutos; i++){
+            String linha = produtosCadastrados[i].gerarDadosTexto();
+            escritor.write(linha);
+            escritor.newLine();
+        }
+
+        escritor.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        teclado = new Scanner(System.in, Charset.forName("ISO-8859-2"));
+        nomeArquivoDados = "oficina/dadosProdutos.csv";
+        produtosCadastrados = lerProdutos(nomeArquivoDados);
+        int opcao = -1;
+        do{
+            opcao = menu();
+            switch (opcao) {
+                case 1 -> listarTodosOsProdutos();
+                case 2 -> localizarProdutos();
+                case 3 -> cadastrarProduto();
+            }
+            pausa();
+        }while(opcao !=0);
+
+        salvarProdutos(nomeArquivoDados);
+        teclado.close();
+    }
+}
--- oficina/src/main/java/com/produtos/Main.java
+++ oficina/src/main/java/com/produtos/Main.java
@@ -1,7 +1,1 @@
 package com.produtos;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
